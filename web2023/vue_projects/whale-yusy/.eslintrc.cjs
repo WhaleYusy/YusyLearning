dffdc4fab51e3418053b6d190c188f1b5d6799a9
WhaleYusy <416705008@qq.com>
2023-06-19 16:54:22 +0800

    updated by 2023.6.19

--- web2023/vue_projects/whale-yusy/.eslintrc.cjs
+++ web2023/vue_projects/whale-yusy/.eslintrc.cjs
@@ -7,9 +7,28 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
   ],
+  rules: {
+    'prefer-const': 2,  // 声明后永远不会重新分配的变量需要 const 声明
+    'semi': [2, 'never'], // 要求或禁止使用分号而不是 ASI
+    'no-var': 2,  // 需要 let 或 const 而不是 var
+    'arrow-spacing': 2, // 在箭头函数中的箭头前后强制执行一致的间距
+    'spaced-comment': 2,  // 在注释中的 // 或 /* 之后强制执行一致的间距
+    'comma-spacing': 2, // 在逗号前后强制执行一致的间距
+    'space-before-blocks': 2, // 在块之前强制执行一致的间距
+    'space-infix-ops': 2, // 要求中缀运算符周围有间距
+    'object-curly-spacing': [2, 'always'], // 在大括号内强制执行一致的间距
+    'space-before-function-paren': 2, // 在 function 定义左括号之前强制执行一致的间距
+    'no-unused-vars': 2,  // 禁止未使用的变量(ts也有检测，不过是warning)
+    'keyword-spacing': 2, // 在关键字前后强制使用一致的间距
+    'no-trailing-spaces': 2, // 不允许在行尾尾随空格
+    'key-spacing': 2, // 在对象字面属性中强制执行键和值之间的一致间距
+    'comma-dangle': [2, {
+      'objects': 'always-multiline',
+      'arrays': 'always-multiline',
+    }],  // 要求或禁止尾随逗号
+  },
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 'latest',
+  },
 }
--- web2023/vue_projects/whale-yusy/src/stores/counter.ts
+++ web2023/vue_projects/whale-yusy/src/stores/counter.ts
@@ -4,9 +4,30 @@ import { defineStore } from 'pinia'
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
-  function increment() {
+  function increment (a:number, b:number, c?:number) {
     count.value++
-  }
+    console.log(a, b, c)
 
+  }
+  const add = (x: number) => x * 2, bdd = 1
+  const a = {
+    name: 'yusy',
+    age: 16,
+    sex: true,
+    obj: {
+      children: [{
+        name: 'xiaojingjing',
+      }],
+    },
+  }
+  console.log(add(2), bdd)
+  console.log(1 | 1, a)
+  console.log(increment(1, 2))
+  if (a.age == 1) {
+    console.log('2')
+  } else {
+    console.log('3')
+  }
+  // 1
   return { count, doubleCount, increment }
 })
