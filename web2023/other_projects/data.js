57d939deb4c621c397262dc6478bb1aec40530a9
WhaleYusy <416705008@qq.com>
2023-08-04 10:09:46 +0800

    updated by 2023.8.4

---
+++ web2023/other_projects/data.js
@@ -0,0 +1,53 @@
+export default [{
+  name: `肖申克的救赎`,
+  nameOther: `The Shawshank Redemption`,
+  year: 1994,
+  releaseDate: `1994-09-10`,
+  imgUrl: '//bkimg.cdn.bcebos.com/pic/e7cd7b899e510fb30f24e313f578df95d143ad4ba656?x-bce-process=image/watermark,image_d2F0ZXIvYmFpa2UxNTA=,g_7,xp_5,yp_5/format,f_auto',
+},{
+  name: `霸王别姬`,
+  nameOther: `Farewell My Concubine`,
+  year: 1993,
+  releaseDate: `1993-01-01`
+},{
+  name: `阿甘正传`,
+  nameOther: `Forrest Gump`,
+  year: 1994,
+  releaseDate: `1994-06-23`
+},{
+  name: `泰坦尼克号`,
+  nameOther: `Titanic`,
+  year: 1997,
+  releaseDate: `1997-11-01`
+},{
+  name: `这个杀手不太冷`,
+  nameOther: `Léon`,
+  year: 1994,
+  releaseDate: `1994-09-14`
+},{
+  name: `千与千寻`,
+  nameOther: `千と千尋の神隠し`,
+  year: 2001,
+  releaseDate: `2001-07-20`
+},{
+  name: `美丽人生`,
+  nameOther: `La vita è bella`,
+  year: 1997,
+  releaseDate: `1997-12-20`
+},{
+  name: `辛德勒的名单`,
+  nameOther: `Schindler's List`,
+  year: 1993,
+  releaseDate: `1993-11-30`
+},{
+  name: `星际穿越`,
+  nameOther: `Interstellar`,
+  year: 2014,
+  releaseDate: `2014-11-07`
+},{
+  name: `盗梦空间`,
+  nameOther: `Inception`,
+  year: 2010,
+  releaseDate: `2010-07-16`
+}]
+
