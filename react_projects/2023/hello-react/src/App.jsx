f2e51c6771e0c4a8f00d4f81a37cc2bd27c38a1a
WhaleYusy <416705008@qq.com>
2023-08-19 17:50:34 +0800

    react update by 2023.8.19

--- react_projects/2023/hello-react/src/App.jsx
+++ react_projects/2023/hello-react/src/App.jsx
@@ -1,28 +1,29 @@
 import React, { Component } from 'react'
-import {nanoid} from 'nanoid'
-import axios from 'axios'
+import { NavLink, Route, Routes } from "react-router-dom";
+import Home from './components/Home'
+import About from './components/About'
+import './App.css'
 
 export default class App extends Component {
-  getStudentData = () => {
-    axios.get('/api1/students').then(res => {
-      console.log(res.data);
-    },
-    err => {
-      console.log(err);
-    })
-    axios.get('/api2/cars').then(res => {
-      console.log(res.data);
-    },
-    err => {
-      console.log(err);
-    })
+  toHome = () => {
+    console.log("to Home");
+  }
+  toAbout = () => {
+    console.log("to About");
   }
   render() {
     return (
       <div>
         <h1>App</h1>
-        <h2>nanoid: {nanoid()}</h2>
-        <button onClick={this.getStudentData}>点我获取学生数据</button>
+          <NavLink className={({isActive}) => isActive ? 'active' : ''} to="/home">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+        <hr />
+        <div>
+          <Routes>
+            <Route path="/about" element={<About />}/>
+            <Route path="/home" element={<Home />}/>
+          </Routes>
+        </div>
       </div>
     )
   }
