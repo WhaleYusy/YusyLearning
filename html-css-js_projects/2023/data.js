7cfb68882d3a017e6e5accc0cd39e336ffe89234
WhaleYusy <416705008@qq.com>
2023-09-18 12:51:05 +0800

    1

--- html-css-js_projects/2023/data.js
+++ html-css-js_projects/2023/data.js
@@ -1,128 +1,202 @@
-export default [{
-  name: `肖申克的救赎`,
-  nameOther: `The Shawshank Redemption`,
-  year: 1994,
-  releaseDate: `1994-09-10`,
-  imgUrl: '//bkimg.cdn.bcebos.com/pic/e7cd7b899e510fb30f24e313f578df95d143ad4ba656?x-bce-process=image/watermark,image_d2F0ZXIvYmFpa2UxNTA=,g_7,xp_5,yp_5/format,f_auto',
-},{
-  name: `霸王别姬`,
-  nameOther: `Farewell My Concubine`,
-  year: 1993,
-  releaseDate: `1993-01-01`
-},{
-  name: `阿甘正传`,
-  nameOther: `Forrest Gump`,
-  year: 1994,
-  releaseDate: `1994-06-23`
-},{
-  name: `泰坦尼克号`,
-  nameOther: `Titanic`,
-  year: 1997,
-  releaseDate: `1997-11-01`
-},{
-  name: `这个杀手不太冷`,
-  nameOther: `Léon`,
-  year: 1994,
-  releaseDate: `1994-09-14`
-},{
-  name: `千与千寻`,
-  nameOther: `千と千尋の神隠し`,
-  year: 2001,
-  releaseDate: `2001-07-20`
-},{
-  name: `美丽人生`,
-  nameOther: `La vita è bella`,
-  year: 1997,
-  releaseDate: `1997-12-20`
-},{
-  name: `辛德勒的名单`,
-  nameOther: `Schindler's List`,
-  year: 1993,
-  releaseDate: `1993-11-30`
-},{
-  name: `星际穿越`,
-  nameOther: `Interstellar`,
-  year: 2014,
-  releaseDate: `2014-11-07`
-},{
-  name: `盗梦空间`,
-  nameOther: `Inception`,
-  year: 2010,
-  releaseDate: `2010-07-16`
-},{
-  name: `楚门的世界`,
-  nameOther: `The Truman Show`,
-  year: 1998,
-  releaseDate: `1998-06-05`
-},{
-  name: `三傻大闹宝莱坞`,
-  nameOther: `3 Idiots`,
-  year: 2009,
-  releaseDate: `2009-12-25`
-},{
-  name: `无名之辈`,
-  nameOther: ``,
-  year: 2018,
-  releaseDate: `2018-11-16`
-},{
-  name: `名侦探柯南：黑铁的鱼影`,
-  nameOther: `名探偵コナン 黒鉄の魚影`,
-  year: 2023,
-  releaseDate: `2023-04-14`
-},{
-  name: `从你的全世界路过`,
-  nameOther: ``,
-  year: 2016,
-  releaseDate: `2016-09-29`
-},{
-  name: `无间道`,
-  nameOther: `無間道`,
-  year: 2002,
-  releaseDate: `2002-12-12`
-},{
-  name: `无间道2`,
-  nameOther: `無間道II`,
-  year: 2003,
-  releaseDate: `2003-10-01`
-},{
-  name: `剑雨`,
-  nameOther: ``,
-  year: 2010,
-  releaseDate: `2010-09-03`
-},{
-  name: `四大名捕`,
-  nameOther: ``,
-  year: 2012,
-  releaseDate: `2012-07-12`
-},{
-  name: `四大名捕2`,
-  nameOther: ``,
-  year: 2013,
-  releaseDate: `2013-12-06`
-},{
-  name: `四大名捕大结局`,
-  nameOther: ``,
-  year: 2014,
-  releaseDate: `2014-08-22`
-},{
-  name: `疯狂动物城`,
-  nameOther: `Zootopia`,
-  year: 2016,
-  releaseDate: `2016-03-04`
-},{
-  name: `大话西游之月光宝盒`,
-  nameOther: `西遊記第壹佰零壹回之月光寶盒`,
-  year: 1995,
-  releaseDate: `1995-01-21`
-},{
-  name: `大话西游之大圣娶亲`,
-  nameOther: `西遊記大結局之仙履奇緣`,
-  year: 1995,
-  releaseDate: `1995-02-04`
-},{
-  name: `疯狂动物城`,
-  nameOther: `Zootopia`,
-  year: 2016,
-  releaseDate: `2016-03-04`
-}]
-
+export default [
+  {
+    name: `肖申克的救赎`,
+    nameOther: `The Shawshank Redemption`,
+    year: 1994,
+    releaseDate: `1994-09-10`,
+    imgUrl:
+      "//bkimg.cdn.bcebos.com/pic/e7cd7b899e510fb30f24e313f578df95d143ad4ba656?x-bce-process=image/watermark,image_d2F0ZXIvYmFpa2UxNTA=,g_7,xp_5,yp_5/format,f_auto",
+  },
+  {
+    name: `霸王别姬`,
+    nameOther: `Farewell My Concubine`,
+    year: 1993,
+    releaseDate: `1993-01-01`,
+  },
+  {
+    name: `阿甘正传`,
+    nameOther: `Forrest Gump`,
+    year: 1994,
+    releaseDate: `1994-06-23`,
+  },
+  {
+    name: `泰坦尼克号`,
+    nameOther: `Titanic`,
+    year: 1997,
+    releaseDate: `1997-11-01`,
+  },
+  {
+    name: `这个杀手不太冷`,
+    nameOther: `Léon`,
+    year: 1994,
+    releaseDate: `1994-09-14`,
+  },
+  {
+    name: `千与千寻`,
+    nameOther: `千と千尋の神隠し`,
+    year: 2001,
+    releaseDate: `2001-07-20`,
+  },
+  {
+    name: `美丽人生`,
+    nameOther: `La vita è bella`,
+    year: 1997,
+    releaseDate: `1997-12-20`,
+  },
+  {
+    name: `辛德勒的名单`,
+    nameOther: `Schindler's List`,
+    year: 1993,
+    releaseDate: `1993-11-30`,
+  },
+  {
+    name: `星际穿越`,
+    nameOther: `Interstellar`,
+    year: 2014,
+    releaseDate: `2014-11-07`,
+  },
+  {
+    name: `盗梦空间`,
+    nameOther: `Inception`,
+    year: 2010,
+    releaseDate: `2010-07-16`,
+  },
+  {
+    name: `楚门的世界`,
+    nameOther: `The Truman Show`,
+    year: 1998,
+    releaseDate: `1998-06-05`,
+  },
+  {
+    name: `三傻大闹宝莱坞`,
+    nameOther: `3 Idiots`,
+    year: 2009,
+    releaseDate: `2009-12-25`,
+  },
+  {
+    name: `无名之辈`,
+    nameOther: ``,
+    year: 2018,
+    releaseDate: `2018-11-16`,
+  },
+  {
+    name: `名侦探柯南：黑铁的鱼影`,
+    nameOther: `名探偵コナン 黒鉄の魚影`,
+    year: 2023,
+    releaseDate: `2023-04-14`,
+  },
+  {
+    name: `从你的全世界路过`,
+    nameOther: ``,
+    year: 2016,
+    releaseDate: `2016-09-29`,
+  },
+  {
+    name: `无间道`,
+    nameOther: `無間道`,
+    year: 2002,
+    releaseDate: `2002-12-12`,
+  },
+  {
+    name: `无间道2`,
+    nameOther: `無間道II`,
+    year: 2003,
+    releaseDate: `2003-10-01`,
+  },
+  {
+    name: `剑雨`,
+    nameOther: ``,
+    year: 2010,
+    releaseDate: `2010-09-03`,
+  },
+  {
+    name: `四大名捕`,
+    nameOther: ``,
+    year: 2012,
+    releaseDate: `2012-07-12`,
+  },
+  {
+    name: `四大名捕2`,
+    nameOther: ``,
+    year: 2013,
+    releaseDate: `2013-12-06`,
+  },
+  {
+    name: `四大名捕大结局`,
+    nameOther: ``,
+    year: 2014,
+    releaseDate: `2014-08-22`,
+  },
+  {
+    name: `疯狂动物城`,
+    nameOther: `Zootopia`,
+    year: 2016,
+    releaseDate: `2016-03-04`,
+  },
+  {
+    name: `大话西游之月光宝盒`,
+    nameOther: `西遊記第壹佰零壹回之月光寶盒`,
+    year: 1995,
+    releaseDate: `1995-01-21`,
+  },
+  {
+    name: `大话西游之大圣娶亲`,
+    nameOther: `西遊記大結局之仙履奇緣`,
+    year: 1995,
+    releaseDate: `1995-02-04`,
+  },
+  {
+    name: `疯狂动物城`,
+    nameOther: `Zootopia`,
+    year: 2016,
+    releaseDate: `2016-03-04`,
+  },
+  {
+    name: `黄飞鸿之英雄有梦`,
+    nameOther: ``,
+    year: 2014,
+    releaseDate: `2014-11-21`,
+  },
+  {
+    name: `我不是药神`,
+    nameOther: ``,
+    year: 2018,
+    releaseDate: `2018-07-05`,
+  },
+  {
+    name: `让子弹飞`,
+    nameOther: ``,
+    year: 2010,
+    releaseDate: `2010-12-16`,
+  },
+  {
+    name: `孤注一掷`,
+    nameOther: ``,
+    year: 2023,
+    releaseDate: `2023-08-08`,
+  },
+  {
+    name: `人生大事`,
+    nameOther: ``,
+    year: 2022,
+    releaseDate: `2022-06-24`,
+  },
+  {
+    name: `功夫`,
+    nameOther: ``,
+    year: 2004,
+    releaseDate: `2004-12-23`,
+  },
+  {
+    name: `喜剧之王`,
+    nameOther: `喜劇之王`,
+    year: 1999,
+    releaseDate: `1999-02-13`,
+  },
+  {
+    name: `唐伯虎点秋香`,
+    nameOther: `唐伯虎點秋香`,
+    year: 1993,
+    releaseDate: `1993-07-01`,
+  },
+];
