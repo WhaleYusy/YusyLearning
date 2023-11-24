26ed415722366418a336402bdb171730d4098417
WhaleYusy <416705008@qq.com>
2023-11-24 17:42:53 +0800

    delete byteDance_projects

--- vue_projects/2022/shangguigu2/src-todos2/util/storageUtil.js
+++
@@ -1,9 +0,0 @@
-const TODOS_KEY = 'todos_key'
-export default{
-  saveTodos(todos){
-    window.localStorage.setItem(TODOS_KEY, JSON.stringify(todos))
-  },
-  readTodos(){
-    return JSON.parse(window.localStorage.getItem(TODOS_KEY) || '[]')
-  }
-}
