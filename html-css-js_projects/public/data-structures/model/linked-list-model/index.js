4af6810147b7098c660c3e69c87f5320af15f19a
WhaleYusy <416705008@qq.com>
2024-01-10 15:32:07 +0800

    2024-01-10 数据结构

---
+++ html-css-js_projects/public/data-structures/model/linked-list-model/index.js
@@ -0,0 +1,19 @@
+function defaultEquals(a, b) {
+  return a === b;
+}
+
+class Node {
+  constructor(element) {
+    this.element = element;
+    this.next = undefined;
+  }
+}
+
+class DoublyNode extends Node{
+  constructor(element, next, prev){
+    super(element, next)
+    this.prev = prev
+  }
+}
+
+module.exports = { defaultEquals, Node, DoublyNode };
