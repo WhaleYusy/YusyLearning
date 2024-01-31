6cae50b3245dfd2db551bdfcf7efc8266474d398
WhaleYusy <416705008@qq.com>
2024-01-31 18:08:16 +0800

    20240131 hello_vue3

---
+++ vue_projects/2024/hello_vue3/备份/01/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app">
+    <h1>你好啊！</h1>  
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+  .app{
+    background-color: #ddd;
+  }
+</style>
---
+++ vue_projects/2024/hello_vue3/备份/02/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app">
+    <h1>你好啊！</h1>
+    <Person />
+  </div>
+</template>
+
+<script lang="ts">
+import Person from './components/Person.vue'
+export default {
+  name: 'App',
+  components: {
+    Person
+  }
+}
+</script>
+
+<style>
+  .app{
+    background-color: #ddd;
+  }
+</style>
