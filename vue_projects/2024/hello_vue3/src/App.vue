9d5f85cbaeecd5a4e0b60288ceb2691324d10752
WhaleYusy <416705008@qq.com>
2024-02-02 13:48:53 +0800

    2024-02-02 hello_vue3

--- vue_projects/2024/hello_vue3/src/App.vue
+++ vue_projects/2024/hello_vue3/src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <!-- <Person /> -->
   <!-- <Car /> -->
-  <Computed />
+  <!-- <Computed /> -->
+  <Watch />
 </template>
 
 <script setup lang="ts">
 import Person from './components/Person.vue'
 import Car from './components/Car.vue'
 import Computed from './components/Computed.vue';
+import Watch from './components/Watch.vue';
 </script>
 
 <style>
