813a50960f597b5019750034c06f1a0fe9d1079a
WhaleYusy <416705008@qq.com>
2024-10-08 09:57:25 +0800

    updated by 2024.10.08

---
+++ VueProjects/vue3-ts-elementplus-20240929/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts" name="App">
+import YuDemo from '@/views/Demo.vue'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+</script>
+
+<template>
+  <el-config-provider :locale="zhCn">
+    <yu-demo />
+  </el-config-provider>
+</template>
+
+<style scoped></style>
---
+++ VueProjects/vue3-ts-elementplus-20240929/src/views/Demo.vue
@@ -0,0 +1,7 @@
+<template>
+  <div id="demo"></div>
+</template>
+<script setup lang="ts" name="Demo">
+import { ref } from 'vue'
+</script>
+<style scoped></style>
